a0fd4ce28f9f04272db6d4fd8a842a0740215018
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-01-21 22:27:01 -0300

    looping to generate a list of cards

--- deck.go
+++ deck.go
@@ -4,6 +4,21 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
 func (cards deck) print() {
 	for index, card := range cards {
 		fmt.Println(index, card)
--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Spades", newCard()}
-	cards = append(cards, "Six of Hearts")
+	cards := newDeck()
 
 	cards.print()
 
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
