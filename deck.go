5ab4e25997135203e763f53a95f6abe7d78284ed
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-01-23 21:28:29 -0300

    fixing cards name order

--- deck.go
+++ deck.go
@@ -8,11 +8,11 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, suit+" of "+value)
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 
